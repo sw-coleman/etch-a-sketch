464ee1ee6af98157aec5106bef83095067bc3506
Sam Coleman <Sam@Sams-MBP-5.lan>
2022-08-30 08:15:58 +1000

    Style changes and add colour selector

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
 
 
-let userInput = 16;
-const canvasSize = userInput * userInput; //Take amount of rows and multiply for total canvas size
+let heightWidth = 16;
+const canvasSize = heightWidth * heightWidth; //Take amount of rows and multiply for total canvas size
+const canvas = document.querySelector('.canvas');
+let paintColour = 'black';
 
 createCanvas(canvasSize);
 
 //Function to create divs for .canvas
 function createCanvas(num){
-    //Reference to .canvas class
-    const canvas = document.querySelector('.canvas');
     //Format canvas to ensure square shape
     canvas.style.cssText = `
-        grid-template-columns: repeat(${userInput}, 1fr);
-        grid-template-rows: repeat(${userInput}, 1fr);`;
+        grid-template-columns: repeat(${heightWidth}, 1fr);
+        grid-template-rows: repeat(${heightWidth}, 1fr);`;
     //For loop to create required divs
     for(i=1; i<=num; i++){
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.setAttribute('style', 'background-color: white');
         canvas.append(pixel);
     }
 }
-//Slider to select cavas size functionality with display that is updated as user uses the slider
-const slider = document.querySelector('.slider')
-const output = document.querySelector('.slider-value')
+
+const pixels = document.querySelectorAll('.pixel');
+
+//Function to reset canvas upon change of canvas size via slider.
+function resetCanvas(){
+}
+
+//Slider to allow user to change canvas size with display that is updated as user uses the slider
+const slider = document.querySelector('.slider');
+const output = document.querySelector('.slider-value');
 output.textContent = `${slider.value} x ${slider.value}`;
 
 slider.oninput = function() {
-    userinput = this.value;
-    //createCanvas(canvasSize); Need to replace with a function that refreshes the canvas all together.
+    heightWidth = this.value;
+    //resetCanvas();
+    //createCanvas(canvasSize);
     output.textContent = `${this.value} x ${this.value}`
 }
 
-//Create nodelist for each pixel to add drawing functionality to canvas.
-const pixels = document.querySelectorAll('.pixel');
 //Change background colour for each pixel from pixels nodelist when 'mouseover' occurs to allow user to draw on canvas.
 pixels.forEach(function(ele) {
         ele.addEventListener('mouseover', highlight);
     function highlight() {
-        ele.setAttribute('style', 'background-color:black')
+        ele.setAttribute('style', `background-color: ${paintColour}`);
     }
 });
-

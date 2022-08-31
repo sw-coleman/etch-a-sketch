3b1557ae98f92c2332ce7eb381dfe7a173d74c09
Sam <samcoleman98@gmail.com>
2022-08-31 16:27:55 +1000

    Add buttons for colour selection, add function to slider to resize canvas

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 let heightWidth = 16;
 const canvasSize = heightWidth * heightWidth; //Take amount of rows and multiply for total canvas size
 const canvas = document.querySelector('.canvas');
@@ -16,9 +15,10 @@ function createCanvas(num){
     //For loop to create required divs
     for(i=1; i<=num; i++){
         const pixel = document.createElement('div');
+        canvas.append(pixel);
         pixel.classList.add('pixel');
         pixel.setAttribute('style', 'background-color: white');
-        canvas.append(pixel);
+        pixel.addEventListener("mouseover", colourPixels)
     }
 }
 
@@ -26,6 +26,8 @@ const pixels = document.querySelectorAll('.pixel');
 
 //Function to reset canvas upon change of canvas size via slider.
 function resetCanvas(){
+    pixels.forEach((pixel) => pixel.remove());
+    createCanvas(canvasSize);
 }
 
 //Slider to allow user to change canvas size with display that is updated as user uses the slider
@@ -35,15 +37,22 @@ output.textContent = `${slider.value} x ${slider.value}`;
 
 slider.oninput = function() {
     heightWidth = this.value;
-    //resetCanvas();
-    //createCanvas(canvasSize);
+    resetCanvas();
     output.textContent = `${this.value} x ${this.value}`
 }
 
 //Change background colour for each pixel from pixels nodelist when 'mouseover' occurs to allow user to draw on canvas.
-pixels.forEach(function(ele) {
+function colourPixels() {
+    pixels.forEach(function(ele) {
         ele.addEventListener('mouseover', highlight);
-    function highlight() {
-        ele.setAttribute('style', `background-color: ${paintColour}`);
-    }
-});
+        function highlight() {
+            ele.setAttribute('style', `background-color: ${paintColour}`);
+        }
+    });
+}
+
+//function to change color upon 'onclick' through HTML button.
+function changeColour(choice) {
+    paintColour = choice;
+}
+    

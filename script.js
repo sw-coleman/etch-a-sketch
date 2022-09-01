ce2a763f4df2104762672693404188a9ac7ba34a
Sam <samcoleman98@gmail.com>
2022-09-01 13:46:15 +1000

    Change canvas size slider to text input and add rainbow colour

--- script.js
+++ script.js
@@ -1,58 +1,60 @@
-
-let heightWidth = 16;
-const canvasSize = heightWidth * heightWidth; //Take amount of rows and multiply for total canvas size
-const canvas = document.querySelector('.canvas');
 let paintColour = 'black';
-
-createCanvas(canvasSize);
 
 //Function to create divs for .canvas
 function createCanvas(num){
-    //Format canvas to ensure square shape
-    canvas.style.cssText = `
-        grid-template-columns: repeat(${heightWidth}, 1fr);
-        grid-template-rows: repeat(${heightWidth}, 1fr);`;
-    //For loop to create required divs
-    for(i=1; i<=num; i++){
+    const canvas = document.querySelector('.canvas');
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => pixel.remove());
+    canvas.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
+    let canvasSize = num*num; //Take amount of rows and multiply for total canvas size
+    //For loop to create empty divs for canvas. 
+    for(i=1; i<=canvasSize; i++){
         const pixel = document.createElement('div');
-        canvas.append(pixel);
+        pixel.addEventListener("mouseover", colourPixels);
+        pixel.style.backgroundColor = 'white';
+        canvas.insertAdjacentElement('beforeend', pixel);
         pixel.classList.add('pixel');
-        pixel.setAttribute('style', 'background-color: white');
-        pixel.addEventListener("mouseover", colourPixels)
     }
 }
 
-const pixels = document.querySelectorAll('.pixel');
+createCanvas(16); //Create 16x16 canvas on loading page.
 
-//Function to reset canvas upon change of canvas size via slider.
-function resetCanvas(){
-    pixels.forEach((pixel) => pixel.remove());
-    createCanvas(canvasSize);
+//Change size of canvas from user input in text box.
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        createCanvas(input);
+        const output = document.querySelector('.output');
+        output.textContent = `${input} x ${input}`;
+    } else {
+        alert("Please select a number between 2 and 100.")
+    }
 }
-
-//Slider to allow user to change canvas size with display that is updated as user uses the slider
-const slider = document.querySelector('.slider');
-const output = document.querySelector('.slider-value');
-output.textContent = `${slider.value} x ${slider.value}`;
-
-slider.oninput = function() {
-    heightWidth = this.value;
-    resetCanvas();
-    output.textContent = `${this.value} x ${this.value}`
-}
-
 //Change background colour for each pixel from pixels nodelist when 'mouseover' occurs to allow user to draw on canvas.
 function colourPixels() {
-    pixels.forEach(function(ele) {
-        ele.addEventListener('mouseover', highlight);
-        function highlight() {
-            ele.setAttribute('style', `background-color: ${paintColour}`);
-        }
-    });
+    if(paintColour == 'rainbow'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = paintColour;   
+    }
 }
-
-//function to change color upon 'onclick' through HTML button.
+//Function to change and display colour upon click of HTML button.
 function changeColour(choice) {
     paintColour = choice;
+    const colourSelection = document.querySelector('.colour-selection');
+    if(choice == 'rainbow') {
+        colourSelection.textContent = `Current colour: Rainbow`; //Converts string to capitalise first letter.
+    } else {
+        colourSelection.textContent = `Current colour: ${paintColour.charAt(0).toUpperCase() + paintColour.slice(1)}`; //Converts string to capitalise first letter.
+    }
 }
+//Clears canvas when resized.
+function resetCanvas() {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => (pixel.style.backgroundColor = "white"));
+}
+
+
+
     
